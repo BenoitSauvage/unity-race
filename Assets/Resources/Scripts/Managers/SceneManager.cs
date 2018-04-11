c8a2d89be413b9355e64af7028cd9ddca4e15e93
Unknown <ligno19@gmail.com>
2018-04-11 10:05:27 -0400

    Scene manager V1.0

---
+++ Assets/Resources/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager {
+
+
+    #region singleton
+    private static SceneManager instance;
+
+    private SceneManager() { }
+
+    public static SceneManager Instance {
+        get {
+            if (instance == null)
+                instance = new SceneManager();
+
+            return instance;
+        }
+    }
+    #endregion singleton
+
+    public string CurrentScene { get; private set; }
+
+
+    public void LoadScene(string sceneName) {
+        CurrentScene = sceneName;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+}
