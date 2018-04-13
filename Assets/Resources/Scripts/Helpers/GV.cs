0da3659529daf5320969f5f4c80b53b4ef2c928d
ChaabaneHatem <hatemchaabane0@gmail.com>
2018-04-13 08:36:16 -0400

    Input Manager
    Reviewed by : Omar
    Input System : Input Manager
    Description :
    Add control for every car one with the keyboard anf the other with a controller
    add some acceleration and control for testing the cars

--- Assets/Resources/Scripts/Helpers/GV.cs
+++ Assets/Resources/Scripts/Helpers/GV.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 using System.Collections;
 
-public class GV : MonoBehaviour {
+public class GV : MonoBehaviour
+{
 
     // GLOBAL VARIABLES
+
+
+
+
+    //car information (player)
+    public static readonly string CAR_TAG = "Car";
+    public static readonly float MAX_AMOUNT_NITRO = 10;
+    public static readonly float MAX_CAR_SPEED = 10;
+
+
 }
