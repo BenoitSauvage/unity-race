618605ec8dde18f488ea95aa934abab751073583
Unknown <ligno19@gmail.com>
2018-04-15 13:24:14 -0400

    AutoCommit

---
+++ Assets/Resources/Scripts/Tools/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility {
+
+    #region Singleton
+    private static Utility instance;
+
+
+   
+    public static Utility Instance {
+        get {
+            if (instance == null) {
+                instance = new Utility();
+            }
+            return instance;
+        }
+    }
+
+    #endregion
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
