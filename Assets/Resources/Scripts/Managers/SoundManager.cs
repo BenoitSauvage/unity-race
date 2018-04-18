11a43ecc41ae7da23db2094d78cae760bccb697b
Unknown <ligno19@gmail.com>
2018-04-18 09:44:44 -0400

    Sound manager begin && Update TimerManager

---
+++ Assets/Resources/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager {
+
+    #region Singleton
+    private static SoundManager instance;
+
+
+    private SoundManager() {
+    }
+
+    public static SoundManager Instance {
+        get {
+            if (instance == null) {
+                instance = new SoundManager();
+            }
+            return instance;
+        }
+    }
+
+    #endregion
+
+    public AudioSource stingSource;
+    public float bpm = 128;
+
+
+
+
+    public void Play() {
+
+       stingSource = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+       AudioClip s1 = Resources.Load<>
+
+       stingSource.Play();
+    }
+  
+}
