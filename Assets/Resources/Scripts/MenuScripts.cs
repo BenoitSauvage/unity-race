80ade31a79ba8afcde3c0a0374d7a00f84418e77
Benoît Sauvage <benoit.sauvage44@gmail.com>
2018-04-23 08:01:51 -0400

    Linked menu to game

---
+++ Assets/Resources/Scripts/MenuScripts.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScripts : MonoBehaviour {
+
+    public void OnStartClick() {
+        FlowManager.Instance.ChangeFlows(GV.SCENENAMES.MainScene);
+    }
+}
